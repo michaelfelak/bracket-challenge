773ed6b729f3da5443260a968f45286d1ed12770
michaelfelak <54033448+michaelfelak@users.noreply.github.com>
2024-01-17 16:56:20 -0500

    add shell components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BracketComponent } from './bracket/bracket.component';
+import { AdminComponent } from './admin/admin.component';
+import { StandingsComponent } from './standings/standings.component';
+import { EntryComponent } from './entry/entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BracketComponent,
+    AdminComponent,
+    StandingsComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bracket/bracket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bracket',
+  templateUrl: './bracket.component.html',
+  styleUrls: ['./bracket.component.scss']
+})
+export class BracketComponent {
+
+}
