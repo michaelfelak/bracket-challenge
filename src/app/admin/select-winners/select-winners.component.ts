d3da8dece7fca4dec3db2eee76b5cf6d00080284
michaelfelak <54033448+michaelfelak@users.noreply.github.com>
2024-03-21 15:24:48 -0400

    add admin updates

--- src/app/admin/select-winners/select-winners.component.ts
+++ src/app/admin/select-winners/select-winners.component.ts
@@ -1,26 +1,135 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BracketService } from 'src/app/shared/services/bracket.service';
-import { Seed } from 'src/app/shared/models/seed';
-import { SkyTokensModule } from '@skyux/indicators';
+import { SkyRepeaterModule } from '@skyux/lists';
+import { AddWinnerRequest, WinnerByRound } from 'src/app/shared/models/winner.model';
 
 @Component({
   selector: 'app-select-winners',
   standalone: true,
-  imports: [CommonModule, SkyTokensModule],
+  imports: [CommonModule, SkyRepeaterModule],
   templateUrl: './select-winners.component.html',
   styleUrls: ['./select-winners.component.scss'],
 })
 export class SelectWinnersComponent implements OnInit {
   @Input()
-  public bracketId!: number;
+  public bracketId: number = 0;
 
-  public seedList: Seed[] | undefined;
+  public round1teamsEast: WinnerByRound[] = [];
+  public round1teamsWest: WinnerByRound[] = [];
+  public round1teamsMidwest: WinnerByRound[] = [];
+  public round1teamsSouth: WinnerByRound[] = [];
+  public round2teamsEast: WinnerByRound[] = [];
+  public round2teamsWest: WinnerByRound[] = [];
+  public round2teamsMidwest: WinnerByRound[] = [];
+  public round2teamsSouth: WinnerByRound[] = [];
+  public round3teamsEast: WinnerByRound[] = [];
+  public round3teamsWest: WinnerByRound[] = [];
+  public round3teamsMidwest: WinnerByRound[] = [];
+  public round3teamsSouth: WinnerByRound[] = [];
+  public round4teamsEast: WinnerByRound[] = [];
+  public round4teamsWest: WinnerByRound[] = [];
+  public round4teamsMidwest: WinnerByRound[] = [];
+  public round4teamsSouth: WinnerByRound[] = [];
+  public round5teams: WinnerByRound[] = [];
+  public round6teams: WinnerByRound[] = [];
+  public champion: WinnerByRound = {};
+
   constructor(private service: BracketService) {}
 
   public ngOnInit() {
-    this.service.getSeedList(this.bracketId!).subscribe((result) => {
-      this.seedList = result;
+    this.refresh();
+  }
+
+  private refresh() {
+    this.service.getSeedList(this.bracketId).subscribe((result) => {
+      if (result) {
+        this.round1teamsEast = result.filter((seed) => {
+          return seed.region_name === 'East';
+        });
+        this.round1teamsWest = result.filter((seed) => {
+          return seed.region_name === 'West';
+        });
+        this.round1teamsMidwest = result.filter((seed) => {
+          return seed.region_name === 'Midwest';
+        });
+        this.round1teamsSouth = result.filter((seed) => {
+          return seed.region_name === 'South';
+        });
+      }
+    });
+
+    this.service.getWinnersByRound(this.bracketId).subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.round2teamsEast = result.filter((foo) => {
+          return foo.round === 1 && foo.region_name === 'East';
+        });
+        this.round3teamsEast = result.filter((foo) => {
+          return foo.round === 2 && foo.region_name === 'East';
+        });
+        this.round4teamsEast = result.filter((foo) => {
+          return foo.round === 3 && foo.region_name === 'East';
+        });
+        this.round2teamsWest = result.filter((foo) => {
+          return foo.round === 1 && foo.region_name === 'West';
+        });
+        this.round3teamsWest = result.filter((foo) => {
+          return foo.round === 2 && foo.region_name === 'West';
+        });
+        this.round4teamsWest = result.filter((foo) => {
+          return foo.round === 3 && foo.region_name === 'West';
+        });
+        this.round2teamsMidwest = result.filter((foo) => {
+          return foo.round === 1 && foo.region_name === 'Midwest';
+        });
+        this.round3teamsMidwest = result.filter((foo) => {
+          return foo.round === 2 && foo.region_name === 'Midwest';
+        });
+        this.round4teamsMidwest = result.filter((foo) => {
+          return foo.round === 3 && foo.region_name === 'Midwest';
+        });
+        this.round2teamsSouth = result.filter((foo) => {
+          return foo.round === 1 && foo.region_name === 'South';
+        });
+        this.round3teamsSouth = result.filter((foo) => {
+          return foo.round === 2 && foo.region_name === 'South';
+        });
+        this.round4teamsSouth = result.filter((foo) => {
+          return foo.round === 3 && foo.region_name === 'South';
+        });
+        this.round5teams = result.filter((foo) => {
+          return foo.round === 4;
+        });
+        this.round6teams = result.filter((foo) => {
+          return foo.round === 5;
+        });
+        this.champion = result.find((foo) => {
+          return foo.round === 6;
+        })!;
+      }
+    });
+  }
+
+  public addWinner(winner: any, round: number) {
+    console.log(winner);
+    console.log('Winner added: ' + winner.school_name + ' round: ' + round);
+    const request: AddWinnerRequest = {
+      bracket_id: this.bracketId,
+      round: round,
+      seed_id: winner.id!,
+    };
+    this.service.addWinner(request).subscribe((result) => {
+      console.log(result);
+      this.refresh();
+    });
+  }
+
+  public removeWinner(winner: any) {
+    console.log(winner);
+    this.service.deleteWinner(winner.winner_id).subscribe((result) => {
+      console.log(result);
+      this.refresh();
     });
   }
 }
