ce1440195c6092ecb608a41b6e01a9638219918e
michaelfelak <54033448+michaelfelak@users.noreply.github.com>
2024-04-03 17:21:26 -0400

    recap 3

--- src/app/recap/recap-4/recap-4.component.ts
+++ src/app/recap/recap-4/recap-4.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SkyBoxModule } from '@skyux/layout';
 
 @Component({
   selector: 'app-recap-4',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SkyBoxModule],
   templateUrl: './recap-4.component.html',
   styleUrls: ['../recap.component.scss'],
 })
