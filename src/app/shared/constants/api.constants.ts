d91023af32ca23eeae8657ae4412b3ca886382e9
michaelfelak <54033448+michaelfelak@users.noreply.github.com>
2026-03-20 16:30:09 -0400

    update constants

--- src/app/shared/constants/api.constants.ts
+++ src/app/shared/constants/api.constants.ts
@@ -3,10 +3,10 @@
  */
 export const API_CONSTANTS = {
   // Production URLs
-  // AUTH_API_URL: 'https://bowl-pickem-144ffdd934e7.herokuapp.com/api/auth',
-  // BRACKET_API_URL: 'https://bowl-pickem-144ffdd934e7.herokuapp.com/api/v1/',
+  AUTH_API_URL: 'https://bowl-pickem-144ffdd934e7.herokuapp.com/api/auth',
+  BRACKET_API_URL: 'https://bowl-pickem-144ffdd934e7.herokuapp.com/api/v1/',
 
   // Local development URLs (commented out)
-  AUTH_API_URL: 'http://localhost:8081/api/auth',
-  BRACKET_API_URL: 'http://localhost:8081/api/v1/',
+  // AUTH_API_URL: 'http://localhost:8081/api/auth',
+  // BRACKET_API_URL: 'http://localhost:8081/api/v1/',
 };
